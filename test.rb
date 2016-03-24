93bac8e05aca621fbcadf1bee7778f7080e59ac0
Mina Kolta <mina.koltaa@gmail.com>
2016-03-25 00:52:10 +0200

    Measuring the whole process step by step using test.rb, nothing takes time or consume memory badly until now, i think it's has to do with read and write to/from csv and sorting

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+require File.expand_path('lib/combiner',File.dirname(__FILE__))
 require 'smarter_csv'
 require 'date'
 def latest(name)
@@ -15,5 +16,40 @@ def latest(name)
 
   files.last
 end
+def combine(merged)
+	result = []
+	merged.each do |_, hash|
+		result << combine_values(hash)
+	end
+	result
+end
+def lazy_read(file)
+	Enumerator.new do |yielder|
+		CSV.foreach(file) do |row|
+			yielder.yield(row)
+		end
+	end
+end	
+KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
+
+combiner = Combiner.new do |value|
+	value[KEYWORD_UNIQUE_ID]
+end
+
 input = latest('project_2012-07-27_2012-10-10_performancedata')
-smart = SmarterCSV.process(input)
+
+puts combiner.combine(lazy_read(input))
+merger = Enumerator.new do |yielder|
+	while true
+		begin
+			list_of_rows = combiner.next
+			merged = combine_hashes(list_of_rows)
+			yielder.yield(combine_values(merged))
+		rescue StopIteration
+			break
+		end
+	end
+end
+
+# smart = SmarterCSV.process(input, {chunk_size: 90})
+
